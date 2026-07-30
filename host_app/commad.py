091663a8c2f89ad6837727a0ebac816fd4590353
DoraPecurlic <dora.pecurlic01@gmail.com>
2026-07-30 20:40:19 +0200

    Add command model

---
+++ host_app/commad.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Optional
+
+class CommandType(Enum):
+    SHOW_TEXT = auto()
+    CLEAR = auto()
+
+@dataclass
+class ParsedCommand:
+    command_type: CommandType
+    text: Optional[str] = None
